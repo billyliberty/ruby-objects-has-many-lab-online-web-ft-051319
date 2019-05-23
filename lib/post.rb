274a3c77d22f4e7a156061f3e550f947d97e5ca1
Joshua Crum <joshua.crum@gmail.com>
2019-05-23 23:46:24 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+require 'pry'
+
+class Post
+
+  attr_accessor :title, :author
+  def initialize(title)
+    @title = title
+  end
+
+  def author_name
+    if @author == nil
+      return nil
+    else
+      @author.name
+    end
+  end
+
+  def self.all
+    @posts
+  end
+end
